efca41292abf6a71ba6bcae816ebf888e8943963
mateustav <mateustav@gmail.com>
2021-01-09 22:01:11 -0800

    Remove react import since create-react-app is taking care of this

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,10 +1,12 @@
-import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import Home from './components/Home';
 import Privacy from './components/Privacy';
 import Oauth from './components/Oauth';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 import './App.css';
 
 const Router = () => (
